bca5438386c7e3cc7c49e8c930592b93b03e2cd0
Feta Fernanda <139522511+fetafernanda@users.noreply.github.com>
2023-07-20 04:42:18 +0700

    Update Register.sol

--- Register.sol
+++ Register.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Register {
+    string public github;
+    address public owner;
+    
+    struct Referral {
+        address referralAddress;
+        string referralString;
+    }
+    
+    Referral[] public referrals;
+    
+    constructor() {
+        github = "fetafernanda";
+        owner = 0xF7C172435a10C7233aDb2209e3FeD42CA0Ec52ea;
+    }
+    
+    function addReferral(address _referralAddress, string memory _referralString) external {
+        require(msg.sender == owner, "Only the owner can add referrals.");
+        referrals.push(Referral(_referralAddress, _referralString));
+    }
+    
+    function totalReferrals() public view returns (uint256) {
+        return referrals.length;
+    }
+}
